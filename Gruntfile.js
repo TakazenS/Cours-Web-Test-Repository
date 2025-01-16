1f95782e6357ed6f2f7ba0187a9aaedd77026d11
Loris Moine <loris.moine1402@gmail.com>
2025-01-16 22:40:26 +0100

    Commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/Gruntfile.js to edit this template
- */
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/gulpfile.js to edit this template
- */
-
 var gulp = require('gulp');
 
 gulp.task('default', function () {
